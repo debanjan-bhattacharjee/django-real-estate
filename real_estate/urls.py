be6e8e2002299aeda062e1c13807d2fc8f75131e
Debanjan Bhattacharjee <debanjan1203@gmail.com>
2023-09-04 19:53:33 +0530

    chore: Split settings

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -1,21 +1,14 @@
-"""real_estate URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+# q: What does settings do here?
+# a: It allows us to access the settings in the settings.py file.
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+# q: What does include do here?
+# a: It allows us to include other URL configurations
+# from other apps in the project.
+from django.conf.urls.static import static
+# q: What does this above line do?
+# a: It allows us to serve static files such as images, CSS, JavaScript, etc.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('supersecret/', admin.site.urls),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
